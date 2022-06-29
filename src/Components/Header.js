45bb8837e3f4f70c14fcda9b957d3260ee7b3735
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-29 17:08:56 -0600

    header styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,20 +2,29 @@ import React from 'react';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import EmailIcon from '@mui/icons-material/Email';
+import '../Styles/Header.css';
 
 const Header = () => {
     return (
-        <div>
+        <div className='top-header'>
             <div className='social-icons'>
-                <InstagramIcon />
+                <InstagramIcon style={{ margin: 10 }} />
                 <TwitterIcon style={{ margin: 10 }} />
-                <EmailIcon />
+                <EmailIcon style={{ margin: 10 }} />
             </div>
             <div className='web-name'>
                 <h1>Name</h1>
             </div>
             <div className='btn'>
                 <button>Subscribe</button>
+            </div>
+            <div className='ham-container'>
+                <input className='checkbox' type='checkbox' name='' id='' />
+                <div className='ham-lines'>
+                    <span className='line line1'></span>
+                    <span className='line line2'></span>
+                    <span className='line line3'></span>
+                </div>
             </div>
         </div>
     )
